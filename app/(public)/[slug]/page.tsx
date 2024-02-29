6274bc34ecb0d95989acacb273ead41a5b965f70
Christian Mack <sfcamack@icloud.com>
2024-02-28 20:44:13 -0500

    testing build without staticParams for production fix

--- app/(public)/[slug]/page.tsx
+++ app/(public)/[slug]/page.tsx
@@ -22,12 +22,10 @@ export default async function DynamicPage({
   );
 }
 
-export async function generateStaticParams() {
-  const pages = await getAllPages();
+// export async function generateStaticParams() {
+//   const pages = await getAllPages();
 
-  if (!pages) return;
-
-  return pages.map((page: any) => ({
-    slug: page.slug,
-  }));
-}
+//   return pages.map((page: any) => ({
+//     slug: page.slug,
+//   }));
+// }
