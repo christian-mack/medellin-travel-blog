b55d55277a4f77b9ef983ed397ae8cde6f126dbc
Christian Mack <sfcamack@icloud.com>
2024-02-29 14:30:30 -0500

    updating nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,38 +2,30 @@
 import React from "react";
 import {
   NavigationMenu,
-  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
-import { cn } from "@/lib/utils";
-import { PaperPlaneIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
 
 export const Nav = ({ pages }: { pages: any[] }) => {
   console.log(pages);
 
   return (
     <NavigationMenu>
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Components</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-              {/* {components.map((component) => (
-                <ListItem
-                  key={component.title}
-                  title={component.title}
-                  href={component.href}
-                >
-                  {component.description}
-                </ListItem>
-              ))} */}
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
+      <NavigationMenuList className="hidden md:flex">
+        {pages.map((page) => {
+          return (
+            <NavigationMenuItem key={page.slug}>
+              <Link href={`/${page.slug}`} legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  {page.title}
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          );
+        })}
       </NavigationMenuList>
     </NavigationMenu>
   );
