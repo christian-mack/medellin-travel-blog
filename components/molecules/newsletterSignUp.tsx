ea90e40d58b8725c50ac4e06f8b349b9e81254bb
Christian Mack <sfcamack@icloud.com>
2024-04-05 19:48:44 -0400

    added loops for email marketing. deploy for production testing

--- components/molecules/newsletterSignUp.tsx
+++ components/molecules/newsletterSignUp.tsx
@@ -1,34 +1,14 @@
-"use client";
 import { inter } from "@/app/fonts";
 import { cn } from "@/lib/utils";
-import React, { useState } from "react";
-
-interface NewsletterSignupState {
-  name: string;
-  email: string;
-  consentGiven: boolean;
-}
+import { loops } from "@/utils/loops";
 
 export default function NewsletterSignUp() {
-  const [formData, setFormData] = useState<NewsletterSignupState>({
-    name: "",
-    email: "",
-    consentGiven: false,
-  });
-
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value, checked, type } = event.target;
-    setFormData({
-      ...formData,
-      [name]: type === "checkbox" ? checked : value,
-    });
-  };
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  async function createNewSubscriber() {
+    "use server";
     // Submit formData to your newsletter service.
-    console.log("Form Data Submitted", formData);
-  };
+    const response = await loops.createContact("sfacmack@icloud.com");
+    console.log("Form Data Submitted", response);
+  }
 
   return (
     <div className="mx-auto my-10 rounded-lg">
@@ -38,22 +18,24 @@ export default function NewsletterSignUp() {
       <p className={cn("text-gray-700 mb-4", inter.className)}>
         Get notified of the best deals on our WordPress themes.
       </p>
-      <form onSubmit={handleSubmit} className={inter.className}>
+      <form action={createNewSubscriber} className={inter.className}>
         <input
           className="w-full mb-4 px-3 py-2 border border-gray-300 rounded"
           type="text"
-          name="name"
-          placeholder="Enter your name"
-          value={formData.name}
-          onChange={handleInputChange}
+          name="firstName"
+          placeholder="Enter your first name"
+        />
+        <input
+          className="w-full mb-4 px-3 py-2 border border-gray-300 rounded"
+          type="text"
+          name="lastName"
+          placeholder="Enter your last name"
         />
         <input
           className="w-full mb-4 px-3 py-2 border border-gray-300 rounded"
           type="email"
           name="email"
           placeholder="Enter your email"
-          value={formData.email}
-          onChange={handleInputChange}
         />
         <button
           type="submit"
@@ -62,13 +44,7 @@ export default function NewsletterSignUp() {
           →
         </button>
         <label className="flex items-center mt-4">
-          <input
-            type="checkbox"
-            name="consentGiven"
-            checked={formData.consentGiven}
-            onChange={handleInputChange}
-            className="mr-2"
-          />
+          <input type="checkbox" name="consentGiven" className="mr-2" />
           By checking this box, you confirm that you have read and are agreeing
           to our terms of use regarding the storage of the data submitted
           through this form.
