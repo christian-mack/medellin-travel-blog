cc68d6fbaf87a5715a7ee5c893eb685d3ed5f2af
Christian Mack <sfcamack@icloud.com>
2024-04-10 16:15:33 -0400

    adding config

--- next.config.js
+++ next.config.js
@@ -14,6 +14,12 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "downloads.ctfassets.net",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
